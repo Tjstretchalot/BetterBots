2f1bfc7dfadaca2b633b47db7bcbae881a9e15a9
Timothy Moore <mtimothy984@gmail.com>
2018-09-15 20:13:40 -0700

    Fix gorges healing clogs

--- lua/bots/gorge/GorgeSelectAnyHurtStructureNode.lua
+++ lua/bots/gorge/GorgeSelectAnyHurtStructureNode.lua
@@ -2,7 +2,7 @@ class 'GorgeSelectAnyHurtStructureNode' (BTNode)
 
 function GorgeSelectAnyHurtStructureNode:Run(context)
   for _, struct in ipairs(GetEntitiesWithMixinForTeam('Construct', context.bot:GetPlayer():GetTeamNumber())) do
-    if struct:GetHealthScalar() < 0.8 then
+    if not struct:isa('Clog') and struct:GetHealthScalar() < 0.8 then
       context.targetId = struct:GetId()
       return self.Success
     end
--- lua/bots/gorge/GorgeSelectVisibleHurtNode.lua
+++ lua/bots/gorge/GorgeSelectVisibleHurtNode.lua
@@ -6,7 +6,7 @@ function GorgeSelectVisibleHurtNode:Run(context)
 
   -- range is pretty small so we can prob see them
   for _, live in ipairs(GetEntitiesWithMixinWithinRange('Live', player:GetOrigin(), 10)) do
-    if live ~= player and live:GetTeamNumber() == team and live:GetIsAlive() and (not live.GetIsBuilt or live:GetIsBuilt()) and live:GetHealthScalar() < 0.97 then
+    if live ~= player and live:GetTeamNumber() == team and live:GetIsAlive() and (not live.GetIsBuilt or live:GetIsBuilt()) and live:GetHealthScalar() < 0.97 and not live:isa('Clog') then
       context.targetId = live:GetId()
       return self.Success
     end
